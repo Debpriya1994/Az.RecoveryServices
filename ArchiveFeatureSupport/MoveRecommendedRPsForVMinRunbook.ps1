7248c9245d9670b91f6cc975fe71017aeabb60c5
Debpriya Bose <67254946+Debpriya1994@users.noreply.github.com>
2021-06-04 14:46:41 +0530

    Create MoveRecommendedRPsForVMinRunbook.ps1
    Move recommended Recovery Points to archive for a VM in Runbook

---
+++ ArchiveFeatureSupport/MoveRecommendedRPsForVMinRunbook.ps1
@@ -0,0 +1,66 @@
+<#
+.SYNOPSIS
+    Moves recommended RPs for a given IaasVM
+.DESCRIPTION
+    Moves recommended RPs for a given IaasVM.
+    This requires PowerShell 7.0 and Az.RecoveryServices preview module installed 
+#>
+
+param( 
+    [Parameter(Mandatory=$true, HelpMessage="SubscriptionId or SubscriptionName ")] 
+    [string] $Subscription, 
+
+    [Parameter(Mandatory=$true)] 
+    [string] $ResourceGroupName,
+
+    [Parameter(Mandatory=$true)] 
+    [string] $VaultName,
+    
+    [Parameter(Mandatory=$true, HelpMessage="Name of Backup Item")] 
+    [String] $VMName
+)
+
+function script:TraceMessage([string] $message, [string] $color="Yellow")
+{
+    Write-Host "`n$message" -ForegroundColor $color
+}
+
+$connection = Get-AutomationConnection -Name AzureRunAsConnection
+
+$connectionResult = Connect-AzAccount `
+-ServicePrincipal `
+-Subscription $Subscription `
+-Tenant $connection.TenantID `
+-ApplicationId $connection.ApplicationID `
+-CertificateThumbprint $connection.CertificateThumbprint
+"Login successful.."
+
+try
+{
+    Set-AzContext -Subscription $Subscription | Out-Null
+}
+catch
+{
+    Add-AzAccount
+    Set-AzContext -Subscription $Subscription | Out-Null
+}
+
+#fetch recovery services vault  
+$vault =  Get-AzRecoveryServicesVault -ResourceGroupName $ResourceGroupName -Name $VaultName
+
+$result = @()    
+
+$BackupItemList = Get-AzRecoveryServicesBackupItem -vaultId $vault.ID -BackupManagementType "AzureVM" -WorkloadType "AzureVM"
+$bckItm = $BackupItemList | Where-Object {$_.Name -match $VMName}
+
+# for each vm item - move all recommended RPs to Archive
+$recRPList = Get-AzRecoveryServicesBackupRecommendedArchivableRPGroup -Item $bckItm -VaultId $vault.ID
+
+foreach ($rp in $recRPList){
+    $job = Move-AzRecoveryServicesBackupRecoveryPoint -RecoveryPoint $rp `
+    -SourceTier $rp.RecoveryPointTier -DestinationTier VaultArchive -VaultId $vault.ID
+
+    $result = $result + $job
+}
+
+Write-Output($result)
