8789ed0aac2753002440387cf52e821766757c63
Himanshu Agarwal <hiaga@microsoft.com>
2021-09-03 10:33:40 +0530

    move archivable RPs in SQL Server

--- ArchiveFeatureSupport/moveArchivableRecoveryPointsForSQLServer.ps1
+++ ArchiveFeatureSupport/moveArchivableRecoveryPointsForSQLServer.ps1
@@ -1,8 +1,8 @@
 <#
 .SYNOPSIS
-    Moves archivable RPs for a given SQL backup item
+    Moves archivable RPs for a given SQL Server
 .DESCRIPTION
-    Moves archivable RPs for a given SQL backup item to the VaultArchive tier. 
+    Moves archivable RPs for a given SQL Server to the VaultArchive tier. 
     By default this script moves archivable RPs for last 2 years.
     This requires PowerShell 7.0 and Az.RecoveryServices preview module installed 
 #>
@@ -24,11 +24,7 @@ param(
     [System.DateTime] $EndDate = (Get-Date).AddDays(0).ToUniversalTime(),
     
     [Parameter(Mandatory=$true, HelpMessage="Name of Virtual Machine")] 
-    [String] $VMName,
-    
-    [Parameter(Mandatory=$true, HelpMessage="Name of DB")] 
-    [String] $DBName
-    
+    [String] $ServerName
 )
 
 function script:TraceMessage([string] $message, [string] $color="Yellow")
@@ -50,36 +46,21 @@ catch
 $vault =  Get-AzRecoveryServicesVault -ResourceGroupName $ResourceGroupName -Name $VaultName
 
 $BackupItemList = Get-AzRecoveryServicesBackupItem -vaultId $vault.ID -BackupManagementType "AzureWorkload" -WorkloadType "MSSQL"
-$bckItm = $BackupItemList | Where-Object {$_.FriendlyName -eq $DBName -and $_.ContainerName -match $VMName}
+$bckItm = $BackupItemList | Where-Object {$_.ServerName -q $ServerName}
 
 # for each sql item - move all move-ready recovery points (wihin given time range) to Archive
-$EndDate1 = $EndDate
-while ($EndDate1 -ge $StartDate) {
-    $timeDiff = ($EndDate1 - $StartDate)
+foreach ($item in $bckItm){
+    $archivableSQLRPs = Get-AzRecoveryServicesBackupRecoveryPoint -Item $item -StartDate $StartDate -EndDate $EndDate -VaultId $vault.ID -IsReadyForMove $true -TargetTier VaultArchive
 
-    if($timeDiff.Days -ge 30){
-        $StartDate1 = $EndDate1.AddDays(-30).ToUniversalTime()
+    if(!($null -eq $archivableSQLRPs)){
+        $result = @()
+        foreach ($rp in $archivableSQLRPs){
+            $job = Move-AzRecoveryServicesBackupRecoveryPoint -RecoveryPoint $rp `
+                -SourceTier VaultStandard -DestinationTier VaultArchive -VaultId $vault.ID
+            
+            $result = $result + $job
+        }
+        
+        Write-Output($result)  
     }
-    else {
-        $StartDate1 = $StartDate
-    }
-
-    $archivableSQLRPs = Get-AzRecoveryServicesBackupRecoveryPoint -Item $bckItm `
-    -StartDate $StartDate1 -EndDate $EndDate1 -VaultId $vault.ID -IsReadyForMove $true `
-    -TargetTier VaultArchive
-
-    $allRecoveryPoints = $allRecoveryPoints + $archivableSQLRPs 
-    
-    $EndDate1 = $EndDate1.AddDays(-30).ToUniversalTime() 
-}       
-
-# for each sql item - move all move-ready recovery points (wihin given time range) to Archive
-$result = @()
-foreach ($rp in $allRecoveryPoints){
-    $job = Move-AzRecoveryServicesBackupRecoveryPoint -RecoveryPoint $rp `
-        -SourceTier $rp.RecoveryPointTier -DestinationTier VaultArchive -VaultId $vault.ID
-    
-    $result = $result + $job
 }
-
-Write-Output($result)
